ba73cf35b7e57eb4901641468c7dab1a6d67a919
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2022-10-04 13:48:53 +0200

    Cleanup

--- VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VisitorsRegistrationSystemBL {
-    public class Class1 {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Description { get; set; }
-
-    }
-}
