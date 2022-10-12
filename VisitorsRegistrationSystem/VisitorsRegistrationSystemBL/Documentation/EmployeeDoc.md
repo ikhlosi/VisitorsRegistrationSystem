c3d16fddadb17c53e88ca0d2de48cade3001d875
tobias wille <tobias.wille@student.hogent.be>
2022-10-12 20:46:54 +0200

    added employeeManager + documentation

---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Documentation/EmployeeDoc.md
@@ -0,0 +1,20 @@
+﻿# Documentation: Employee
+A class made to model a real life employee within a company in code. The 'Employee' class stores the necessary attributes to instantiate an object of type 'Employee'.
+## Attributes
+*Name (string): the first name of an employee
+*LastName (string): the last name of an employee
+*Email (string): the email of an employee
+*function (string): the function of an employee withing the company
+
+## Methods
+This class only has methods which set its attributes (setters) and constructors.
+
+### Constructor
+The constructor has no overload.
+* Constructor 1: Employee(string name, string lastName, string email, string function)
+### Setters
+* SetName(string name) : void. Sets the Name attribute. Throws an exception if the argument is `null` or is an empty string or a string with only whitespace characters.
+* SetLastName(string lastname) : void. Sets the LastName attribute. Throws an exception if the argument is `null` or is an empty string or a string with only whitespace characters.
+* SetEmail(string email) : void. Sets the Email attribute. Throws an exception if the argument is `null` or is an empty string or a string with only whitespace characters.
+* SetFunction(string function) : void. Sets the Function attribute. Throws an exception if the argument is `null` or is an empty string or a string with only whitespace characters.
+
---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Managers/EmployeeManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorsRegistrationSystemBL.Domain;
+using VisitorsRegistrationSystemBL.Exceptions;
+
+namespace VisitorsRegistrationSystemBL.Managers
+{
+    internal class EmployeeManager
+    {
+        private Dictionary<string, Employee> _employees = new Dictionary<string, Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee == null) throw new VisitorException("EmployeeManager - Addemployee - employee is null");
+            if (_employees.ContainsKey(employee.Name)) throw new VisitorException("EmManager - Addemployee - employee has already been registered");
+            _employees.Add(employee.Name, employee);
+        }
+        public void DeleteEmployee(Employee employee)
+        {
+            if (employee == null) throw new VisitorException("EmployeeManager - DeleteEmployee - visitor is null");
+            if (!_employees.ContainsKey(employee.Name)) throw new VisitorException(" EmployeeManager - DeleteEmployee - employee is not registered");
+            _employees.Remove(employee.Name);
+        }
+        public void UpdateEmployee(Employee employee)
+        {
+            if (employee == null) throw new VisitorException("VisitorManager - UpdateVisitor - visitor is null");
+            if (!_employees.ContainsKey(employee.Name)) throw new VisitorException("VisitorManager - UpdateVisitor - visitor is not registered");
+            if (_employees[employee.Name].Equals(employee)) throw new VisitorException("VisitorManager - UpdateVisitor - updated visitor is unchanged");
+            _employees[employee.Name] = employee;
+        }
+        public IReadOnlyList<Employee> GetEmployees()
+        {
+            return _employees.Values.ToList().AsReadOnly();
+        }
+    }
+}
