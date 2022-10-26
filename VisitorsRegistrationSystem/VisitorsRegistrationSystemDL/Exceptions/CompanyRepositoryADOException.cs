343f0ab79dc609c8945a77710f87b62574fdcad5
ArnoVantieghem <arno.vantieghem@student.hogent.be>
2022-10-26 20:47:24 +0200

    CompanyRepositoryADO initial commit - TODO:write queries (need DB first)

---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemDL/Exceptions/CompanyRepositoryADOException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorsRegistrationSystemDL.Exceptions
+{
+    public class CompanyRepositoryADOException : Exception
+    {
+        public CompanyRepositoryADOException()
+        {
+        }
+
+        public CompanyRepositoryADOException(string? message) : base(message)
+        {
+        }
+
+        public CompanyRepositoryADOException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CompanyRepositoryADOException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
