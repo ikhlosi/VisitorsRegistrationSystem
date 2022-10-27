ceabdd2978c9cb3d86fd11f18699644f84eecdd3
tobias wille <tobias.wille@student.hogent.be>
2022-10-27 11:06:05 +0200

    repo

--- VisitorsRegistrationSystem/VisitorsRegistrationSystemDL/Repositories/VisitRepositoryADO.cs
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemDL/Repositories/VisitRepositoryADO.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VisitorsRegistrationSystemBL.Domain;
+using VisitorsRegistrationSystemBL.Interfaces;
 
 namespace VisitorsRegistrationSystemDL.Repositories
 {
-    internal class VisitRepositoryADO
+    internal class VisitRepositoryADO : IVisitRepository
     {
+        private string _connectionString;
+     public VisitRepositoryADO(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddVisit(Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Visitor GetVisit(int iD)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVisit(Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVisit(Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VisitExists(Visit visit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VisitExists(int iD)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
