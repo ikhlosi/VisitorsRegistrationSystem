4d9e0be0ecca3ca7a17931a121230605ce561065
tobias wille <tobias.wille@student.hogent.be>
2022-11-02 11:14:51 +0100

    Address tests

--- VisitorsRegistrationSystem/TestDomain/AddressTest.cs
+++ VisitorsRegistrationSystem/TestDomain/AddressTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using VisitorsRegistrationSystemBL.Domain;
+using VisitorsRegistrationSystemBL.Exceptions;
 using VisitorsRegistrationSystemBL.Factories;
 
 namespace TestDomain
@@ -11,16 +13,65 @@ namespace TestDomain
     public class AddressTest
     {
         [Fact]
-        public void SetCit_valid()
+        public void SetCity_valid()
         {
             Address address = new Address("Gent","sleepstraat","5",null);
             address.SetCity("Gent");
             Assert.Equal("Gent", address.City);
         }
-        [Fact]
-        public void SetStreet_valid()
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void SetCity_Invalid(string city)
         {
+            Address address = new Address("Gent", "sleepstraat", "5", null);
+            Assert.Throws<AddressException>(() => address.SetCity(city));
+
 
         }
+            [Fact]
+        public void SetStreet_valid()
+        {
+            Address address = new Address("Gent", "sleepstraat", "5", null);
+            address.SetStreet("sleepstraat");
+            Assert.Equal("sleepstraat", address.Street);
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void SetStreet_Invalid(string street)
+        {
+            Address address = new Address("Gent", "sleepstraat", "5", null);
+            Assert.Throws<AddressException>(() => address.SetStreet(street));
+
+
+        }
+        [Fact]
+        public void SetHouseNo_valid()
+        {
+            Address address = new Address("Gent", "sleepstraat", "5", null);
+            address.SetHouseNo("5");
+            Assert.Equal("5", address.HouseNumber);
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void SetHouseNo_Invalid(string houseNo)
+        {
+            Address address = new Address("Gent", "sleepstraat", "5", null);
+            Assert.Throws<AddressException>(() => address.SetHouseNo(houseNo));
+
+
+        }
+        [Fact]
+        public void SetBusNo_valid()
+        {
+            Address address = new Address("Gent", "sleepstraat", "5", "502");
+            address.SetBusNo("505");
+            Assert.Equal("505", address.BusNumber);
+        }
+       
+       
+
     }
 }
