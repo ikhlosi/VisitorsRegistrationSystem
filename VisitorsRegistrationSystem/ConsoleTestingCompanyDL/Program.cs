0b2957aa9c0b4c19ef4594ee5caac8fb6e4c0f74
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2022-11-02 14:10:32 +0100

    organising

--- VisitorsRegistrationSystem/ConsoleTestingCompanyDL/Program.cs
+++ VisitorsRegistrationSystem/ConsoleTestingCompanyDL/Program.cs
@@ -5,8 +5,11 @@ using VisitorsRegistrationSystemDL.Repositories;
 
 Console.WriteLine("Hello, World!");
 // !!! vergeet connectionstring niet aan te passen !!!
-string connectionString = @"Data Source=laptop-hfkukp2u\sqlexpress;Initial Catalog=VisitorsRegistrationSystem;Integrated Security=True";
-CompanyRepositoryADO cRepo = new CompanyRepositoryADO(connectionString);
+string connectionStringArno = @"Data Source=laptop-hfkukp2u\sqlexpress;Initial Catalog=VisitorsRegistrationSystem;Integrated Security=True";
+string connectionStringTobias = @"Data Source=DESKTOP-91NOPN3\SQLEXPRESS;Initial Catalog=VisitorsRegistrationSystem;Integrated Security=True";
+string connectionStringIbra = @"Data Source=DESKTOP-QT687QR\SQLEXPRESS;Initial Catalog=VisitorsRegistrationSystem;Integrated Security=True";
+string connectionStringPetar = null;
+CompanyRepositoryADO cRepo = new CompanyRepositoryADO(connectionStringArno);
 Company company = CompanyFactory.MakeCompany(null,"Brightest","1234567890",new Address("Elsegem","Kouterlos","2"),"0479564641","arnovantieghem@gmail.com");
 
 Console.WriteLine(cRepo.CompanyExistsInDB(company));
