1fde270b8aa61962a5729c54a9f4a87c2dedcdd6
petarkuga3 <32229416+petarkuga3@users.noreply.github.com>
2022-11-22 12:13:14 +0100

    Beheer GUI update

--- VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Exceptions/DomainException.cs
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Exceptions/DomainException.cs
@@ -4,19 +4,25 @@
 //using System.Text;
 //using System.Threading.Tasks;
 
-//namespace VisitorsRegistrationSystemBL.Exceptions {
-//    public class DomainException : Exception {
+//namespace VisitorsRegistrationSystemBL.Exceptions
+//{
+//    public class DomainException : Exception
+//    {
 //        private string _exceptionInfo = $"{this.GetType().Name}";
-//        public DomainException() {
+//        public DomainException()
+//        {
 //        }
 
-//        public DomainException(string? message) : base($"{GenerateExceptionInfo()} {System.Reflection.MethodBase.GetCurrentMethod().Name} - " + message) {
+//        public DomainException(string? message) : base($"{GenerateExceptionInfo()} {System.Reflection.MethodBase.GetCurrentMethod().Name} - " + message)
+//        {
 //        }
 
-//        public DomainException(string? message, Exception? innerException) : base(message, innerException) {
+//        public DomainException(string? message, Exception? innerException) : base(message, innerException)
+//        {
 //        }
 
-//        private string GenerateExceptionInfo() {
+//        private string GenerateExceptionInfo()
+//        {
 //            return $"{this.GetType()}:";
 //        }
 //    }
