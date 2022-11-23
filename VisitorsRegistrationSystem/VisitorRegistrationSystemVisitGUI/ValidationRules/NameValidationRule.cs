a7589382436cface68673e373ea0841de073c3e3
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2022-11-23 10:16:26 +0100

    Validation Rule for Names
    Created a validation rule to validate names (first name, last name,
    visitor's company name). Handled the correct namespaces for the data
    context and data binding

---
+++ VisitorsRegistrationSystem/VisitorRegistrationSystemVisitGUI/ValidationRules/NameValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VisitorRegistrationSystemVisitGUI.ValidationRules {
+    internal class NameValidationRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            string valueToValidate = value as string;
+            if (string.IsNullOrWhiteSpace(valueToValidate) ) {
+                return new ValidationResult(false, "Mag niet leeg zijn");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
