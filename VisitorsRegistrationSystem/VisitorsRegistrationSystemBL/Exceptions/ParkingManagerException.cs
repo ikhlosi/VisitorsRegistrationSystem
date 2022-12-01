2607be769420b2e6339a28bdf0d27430560a168a
ArnoVantieghem <arno.vantieghem@student.hogent.be>
2022-12-01 10:54:14 +0100

    parking - initial classes

---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Exceptions/ParkingManagerException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorsRegistrationSystemBL.Exceptions
+{
+    public class ParkingManagerException : Exception
+    {
+        public ParkingManagerException()
+        {
+        }
+
+        public ParkingManagerException(string? message) : base(message)
+        {
+        }
+
+        public ParkingManagerException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ParkingManagerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemDL/Exceptions/ParkingRepositoryADOException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorsRegistrationSystemDL.Exceptions
+{
+    public class ParkingRepositoryADOException : Exception
+    {
+        public ParkingRepositoryADOException()
+        {
+        }
+
+        public ParkingRepositoryADOException(string? message) : base(message)
+        {
+        }
+
+        public ParkingRepositoryADOException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ParkingRepositoryADOException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
