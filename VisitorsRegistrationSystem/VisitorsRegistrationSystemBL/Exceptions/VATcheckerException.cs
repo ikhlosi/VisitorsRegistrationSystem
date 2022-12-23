557efa96847c6c03c1d98e89aaf5a821468ba066
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2022-12-23 02:30:59 +0100

    Implemented VAT Checker

---
+++ VisitorsRegistrationSystem/VisitorsRegistrationSystemBL/Exceptions/VATcheckerException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace VisitorsRegistrationSystemBL.Exceptions
+{
+    [Serializable]
+    internal class VATcheckerException : Exception
+    {
+        public VATcheckerException()
+        {
+        }
+
+        public VATcheckerException(string? message) : base(message)
+        {
+        }
+
+        public VATcheckerException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected VATcheckerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
